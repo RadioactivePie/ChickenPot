e93c69efd7686e45516f7e4e55ab640d78c71fe3
Ronnie Lusso <Ronnielusso@gmail.com>
2016-05-09 09:05:42 -0400

    Add files via upload

---
+++ Mouse      Control/KeyControl.java
@@ -0,0 +1,68 @@
+
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyControl implements KeyListener {
+	public boolean pressed = false;
+	public boolean right = false;
+	public boolean space = false;
+	public boolean down = false;
+	public boolean left = false;
+	public boolean up = false;
+
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// controls
+		if (e.getKeyCode() == KeyEvent.VK_D) {
+			right = true;
+			pressed = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_A) {
+			left = true;
+			pressed = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_W) {
+			up = true;
+			pressed = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_S) {
+			down = true;
+			pressed = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+			space = true;
+		}
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_D) {
+			right = false;
+			pressed = false;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_A) {
+			left = false;
+			pressed = false;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_W) {
+			up = false;
+			pressed = false;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_S) {
+			down = false;
+			pressed = false;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+			space = false;
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+	}
+}
