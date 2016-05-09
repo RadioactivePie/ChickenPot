e9d511a823582726207e749e4e7e2e03af6cce65
Ronnie Lusso <Ronnielusso@gmail.com>
2016-05-09 10:48:29 -0400

    Update KeyControl.java

--- Help/KeyControl.java
+++ Help/KeyControl.java
@@ -4,7 +4,14 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyControl implements KeyListener {
-
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// Example: Check if "D" was Clicked
+		if (e.getKeyCode() == KeyEvent.VK_D) {
+			// Do something 
+		}
+	}
+	
 	@Override
 	public void keyPressed(KeyEvent e) {
 		
